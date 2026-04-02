c32b219e2e00655de2bd71678eaf715314996fa2
Kelvin Xavier <kelvin.xavier5@gmail.com>
2026-04-02 19:02:39 -0300

    Incluindo certeza do cliente que disse que 10 divido por zero é zero

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+
+def somar(valor1, valor2):
+    return valor1 + valor2 + 5
+
+def subtrair(valor1, valor2):
+    return valor1 - valor2
+
+def multiplicar(valor1, valor2):
+    return valor1 * valor2
+
+def dividir(valor1, valor2):
+    return valor1 / valor2
+
+
+def dar_boas_vindas_ao_aluno(nome):
+    return f'Olá, {nome}!'
---
+++ test_calc.py
@@ -0,0 +1,15 @@
+from calculadora import *
+import pytest 
+
+def test_somar_valores_10_e_20():
+    assert somar(10, 20) == 35
+    
+def test_subtrair_valores_negativos():
+    assert subtrair(-1, -1) == 0
+    
+def test_cumprimentar_aluno_com_boas_vindas():
+    assert dar_boas_vindas_ao_aluno("kelvin") \
+            == "Olá, kelvin!"
+            
+def test_dividir_valor_por_zero():
+    assert dividir(10, 0) == 0
